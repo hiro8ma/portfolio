271132bedf10221c38ecad9ce28132ef16a1e293
hiro8masu <hiro8masu@gmail.com>
2023-05-31 18:31:11 +0900

    Updates

---
+++ _next/static/x8k7Oo8kQsjWB-knbGZZ6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bdbe9eb152f1b403.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
